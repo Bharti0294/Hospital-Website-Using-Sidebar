f9e1fa4602f5e41b051f99656e8706808c143783
Bharti0294 <bharti.nehul07@gmail.com>
2021-05-04 14:33:35 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,92 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { AboutusComponent} from './aboutus/aboutus.component';
+import {ServiceComponent} from './service/service.component';
+import {AppointmentComponent} from './appointment/appointment.component';
+import {ContactComponent} from './contact/contact.component';
+import {DoctorsComponent} from './doctors/doctors.component';
+import {TeamComponent} from './team/team.component';
+import {DepartmentComponent} from './department/department.component';
+import {TestimonialsComponent} from './testimonials/testimonials.component';
+import {FacilitiesComponent} from './facilities/facilities.component';
+import {PricingComponent} from './pricing/pricing.component';
+import {GallaryComponent} from './gallary/gallary.component';
+import {BlogComponent} from './blog/blog.component';
+import {ReviewComponent} from './review/review.component';
+import {LocationComponent} from './location/location.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path:"home",
+    component:HomeComponent
+  },
+
+  {
+    path:"aboutus",
+    component:AboutusComponent
+  },
+  
+  {
+    path:"service",
+    component:ServiceComponent
+  },
+  
+  {
+    path:"appointment",
+    component:AppointmentComponent
+  },
+  
+  {
+    path:"contact",
+    component:ContactComponent
+  },
+
+  
+  {
+    path:"doctors",
+    component:DoctorsComponent
+  },
+  {
+    path:"team",
+    component:TeamComponent
+  },
+  {
+    path:"department",
+    component:DepartmentComponent
+  },
+  {
+    path:"testimonials",
+    component:TestimonialsComponent
+  },
+  {
+    path:"facilities",
+    component:FacilitiesComponent
+  },
+  {
+    path:"pricing",
+    component:PricingComponent
+  },
+  {
+    path:"gallary",
+    component:GallaryComponent
+  },
+  {
+    path:"blog",
+    component:BlogComponent
+  },
+  
+  {
+    path:"review",
+    component:ReviewComponent
+  },
+  {
+    path:"location",
+    component:LocationComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { TeamComponent } from './team/team.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { FacilitiesComponent } from './facilities/facilities.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { DepartmentComponent } from './department/department.component';
+import { BlogComponent } from './blog/blog.component';
+import { GallaryComponent } from './gallary/gallary.component';
+import { LocationComponent } from './location/location.component';
+import { ReviewComponent } from './review/review.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ServiceComponent,
+    ContactComponent,
+   AppointmentComponent,
+   
+   TeamComponent,
+   TestimonialsComponent,
+   FacilitiesComponent,
+   PricingComponent,
+   DepartmentComponent,
+   BlogComponent,
+   GallaryComponent,
+   LocationComponent,
+   ReviewComponent,
+   DoctorsComponent,
+   FooterComponent,
+   SidebarComponent,
+   
   ],
   imports: [
     BrowserModule,
